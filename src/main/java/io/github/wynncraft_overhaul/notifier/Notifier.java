cbdff11e219bfeb1a9526dffd32b98762ab6c78a
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-04-05 21:14:48 +0200

    Rebrand to `notifier`

---
+++ src/main/java/io/github/wynncraft_overhaul/notifier/Notifier.java
@@ -0,0 +1,12 @@
+package io.github.wynncraft_overhaul.notifier;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Notifier implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("notifier");
+
+	@Override
+	public void onInitialize() {}
+}
